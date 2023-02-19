e63ec0dcbbf3bb1ea55e54fcbf6f9f6fc03dc239
Puggan Sundragon <thorbjorn.sundragon@rolfsbuss.se>
2023-02-19 07:46:34 +0100

    add json schema

--- build.sh
+++ build.sh
@@ -4,3 +4,10 @@ mkdir -p build
 tsc -p tsconfig.json
 lessc src/style.less build/style.css
 cp src/index.html src/data.json build/
+if command -v typescript-json-schema &> /dev/null
+then
+  typescript-json-schema src/data.d.ts typeSupportData > typeSupportData.schema.json
+else
+  echo "typeSupportData.schema.json not rebuilt, didn't find typescript-json-schema" >&2
+fi
+
